61ea0eb2c2f501995621e3dd0f93fb33d8e01bd1
JessieXie111 <82140642+JessieXie111@users.noreply.github.com>
2023-05-06 14:05:18 +0800

    Chat page left part basic

---
+++ flask/static/dog_select.js
@@ -0,0 +1,22 @@
+const tabsParentContainer = document.querySelector(".dog-tab-parent");
+const tabsMarker = tabsParentContainer.querySelector(".tab_marker");
+const visibleTab = tabsParentContainer.querySelector(
+   ".tab input:checked + label"
+);
+
+
+// listen user clicks on tabs
+tabsParentContainer.addEventListener("change", (e) => {
+   let input = e.target;
+   
+   // toggle visible content
+   let existingVisibleContent = tabsParentContainer.querySelector(
+      ".tab_content.visible"
+   );
+   existingVisibleContent.classList.remove("visible");
+   let associatedTabContent = tabsParentContainer.querySelector(
+      `.tab_content[data-associated-tab="${input.id}"]`
+   );
+   associatedTabContent.classList.add("visible");
+
+});
